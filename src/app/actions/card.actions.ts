bc077051249b5b9b93dee475e5a4ae09dc69b71c
Avishek Datta Ray <avishek.arunavo1997@gmail.com>
2021-01-26 21:03:35 +0530

    Update card.actions.ts

--- src/app/actions/card.actions.ts
+++ src/app/actions/card.actions.ts
@@ -2,7 +2,6 @@ import { Action } from '@ngrx/store';
 import { CardDetails } from '../model/card-details.model';
 
 export const ADD_DETAIL = '[DETAIL] Add';
-// export const DELETE_DETAIL = '[DETAIL] Delete';
 
 export class AddDetail implements Action {
     readonly type = ADD_DETAIL;
